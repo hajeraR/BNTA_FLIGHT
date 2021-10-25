fd0cdd988fe088fcc3eff716b754f4c6bb002500
Hajera Rahman <hajerarahman24@gmail.com>
2021-10-25 10:23:32 +0100

    uses one class

--- Flight/src/main/java/com/psychedelicairlines/Flights/FlightBookingService.java
+++ Flight/src/main/java/com/psychedelicairlines/Flights/FlightBookingService.java
@@ -1,14 +1,19 @@
 package com.psychedelicairlines.Flights;
 
+import com.psychedelicairlines.Passenger.Gender;
+import com.psychedelicairlines.Passenger.ID;
+import com.psychedelicairlines.Passenger.Passenger;
 import com.psychedelicairlines.Passenger.PassengerService;
 
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class FlightBookingService {
 
-    private String country;
+
+//    private String country;
 
     FlightDetails peru = new FlightDetails("Llama", "We ran out of money...", "PE-234", FlightCountry.PERU, FlightCountry.ENGLAND, LocalDateTime.of(2021, Month.NOVEMBER, 12, 13, 47), 957.00, 20, 0);
 
@@ -26,7 +31,7 @@ public class FlightBookingService {
         FlightBookingService flightBookingService = new FlightBookingService();
         System.out.println("");
         System.out.println("Welcome to the Psychedelic Airline experience! Choose an option from below:");
-        System.out.println("\n Option 1 - Book new flight \n Option 2 - Manage Booking \n (1/2)");
+        System.out.println("\n Option 1 - Book new flight \n Option 2 - Manage Booking \n Option 3 - EXIT");
         Scanner scanner =new Scanner(System.in);
         String input = scanner.nextLine();
 
@@ -36,6 +41,10 @@ public class FlightBookingService {
                 break;
             case "2":
                 System.out.println("Manage Booking");
+                break;
+            case "3":
+                System.out.println("Exs--ter--min--ate!");
+                System.exit(0);
                 break;
         }
     }
@@ -70,10 +79,13 @@ public class FlightBookingService {
 
                     case "1":
                         if (peru.getBookedSeats() + 1 < peru.getCapacity()){
-                            passengerService.enterPassengerDetails();
-                            String country = FlightCountry.PERU.toString();
+                            int countPeru = peru.getCapacity();
+                            peru.setCapacity(++countPeru);
+                            enterPassengerDetailsPeru();
+
+                            String countryPeru = peru.getDestination().toString();
                             System.out.println("");
-                            System.out.println("Your flight to " + country + " is confirmed!");
+                            System.out.println("Your flight to " + countryPeru + " is confirmed!");
                             System.out.println("");
                             System.out.println("Thank you for taking part in the Psychedelic Airlines Experience!");
 
@@ -108,10 +120,12 @@ public class FlightBookingService {
 
                     case "1":
                         if (madagascar.getBookedSeats() + 1 < madagascar.getCapacity()){
-                            passengerService.enterPassengerDetails();
-                            String country = FlightCountry.MADAGASCAR.toString();
+                            int countMadagascar = peru.getCapacity();
+                            peru.setCapacity(++countMadagascar);
+                            enterPassengerDetailsMadagascar();
+                            String countryMadagascar = madagascar.getDestination().toString();
                             System.out.println("");
-                            System.out.println("Your flight to " + country + " is confirmed!");
+                            System.out.println("Your flight to " + countryMadagascar + " is confirmed!");
                             System.out.println("");
                             System.out.println("Thank you for taking part in the Psychedelic Airlines Experience!");
                         } else System.out.println("Plane fully booked! You can no longer mingle with Madagascar's Malagasy giant rat!");
@@ -145,10 +159,13 @@ public class FlightBookingService {
 
                     case "1":
                         if (saudi.getBookedSeats() + 1 < saudi.getCapacity()){
-                            passengerService.enterPassengerDetails();
-                            String country = FlightCountry.SAUDI.toString();
+                            int count = saudi.getCapacity();
+                            saudi.setCapacity(++count);
+                            enterPassengerDetailsSaudi();
+
+                            String countrySaudi = saudi.getDestination().toString();
                             System.out.println("");
-                            System.out.println("Your flight to " + country + " is confirmed!");
+                            System.out.println("Your flight to " + countrySaudi + " is confirmed!");
                             System.out.println("");
                             System.out.println("Thank you for taking part in the Psychedelic Airlines Experience!");
                         } else System.out.println("Plane fully booked! Can't taste no Arabian delights. Hah, sucks to be you!");
@@ -182,10 +199,13 @@ public class FlightBookingService {
 
                     case "1":
                         if (australia.getBookedSeats() + 1 < australia.getCapacity()){
-                            passengerService.enterPassengerDetails();
-                            String country = FlightCountry.AUSTRALIA.toString();
+                            int countAustralia = australia.getCapacity();
+                            peru.setCapacity(++countAustralia);
+                            enterPassengerDetailsAustralia();
+
+                            String countryAustralia = australia.getDestination().toString();
                             System.out.println("");
-                            System.out.println("Your flight to " + country + " is confirmed!");
+                            System.out.println("Your flight to " + countryAustralia + " is confirmed!");
                             System.out.println("");
                             System.out.println("Thank you for taking part in the Psychedelic Airlines Experience!");
                         } else System.out.println("Plane fully booked! No surfing those beautiful sandy waves for you!");
@@ -217,13 +237,228 @@ public class FlightBookingService {
 
 
 
-    public String getCountry() {
-        return country;
+    public void enterPassengerDetailsPeru(){
+        FlightBookingService flightBookingService = new FlightBookingService();
+
+        Scanner details = new Scanner(System.in);
+        System.out.println("Prove you're not a robot; enter your destination");
+        String countryPeru1 = details.nextLine();
+
+        if (countryPeru1.toUpperCase(Locale.ROOT).equals("PERU")) {
+            //FlightCountry countryPeruDestination = FlightCountry.valueOf(countryPeru1.toUpperCase(Locale.ROOT));
+            String countryPeruDestination = countryPeru1.toUpperCase(Locale.ROOT);
+
+            System.out.println("Please enter your name:");
+            String name = details.nextLine();
+            //Passenger passenger1 = new Passenger(name);
+            System.out.println("Please enter your email:");
+            String email = details.nextLine();
+
+            System.out.println("Please enter your gender:");
+            String gender1 = details.nextLine();
+            Gender gender = Gender.valueOf(gender1);
+
+            System.out.println("Please enter your Passport Number:");
+            String passport = details.nextLine();
+
+            System.out.println("Please enter your Phone Number:");
+            Integer mobile = details.nextInt();
+
+            System.out.println("");
+            ID idGenerate = new ID();
+            StringBuilder id = idGenerate.generateId("ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890", 6);
+
+
+            //FlightBookingService service = new FlightBookingService();
+            //String country = service.getCountry();
+
+            //show all of the flight details and passenger details and ideally send this all in an email to user:
+            System.out.println("");
+            System.out.println("Here are your personal details:");
+            System.out.println("");
+            Passenger passenger = new Passenger(id, countryPeruDestination, name, email, gender, mobile, passport);
+            System.out.println(passenger.toString());
+            System.out.println("");
+            System.out.println("May The Odds Be Ever In Your Favour!");
+
+        } else {
+            System.out.println("GOT YA, ROBOT!");
+            System.out.println("");
+            flightBookingService.startBooking();
+
+        }
+
+
+
     }
 
-    public void setName(String country) {
-        this.country = country;
+
+    public void enterPassengerDetailsMadagascar() {
+        FlightBookingService flightBookingService = new FlightBookingService();
+
+        Scanner details = new Scanner(System.in);
+        System.out.println("Prove you're not a robot; enter your destination");
+        String countryMadagascar1 = details.nextLine();
+
+        if (countryMadagascar1.toUpperCase(Locale.ROOT).equals("MADAGASCAR")) {
+            //FlightCountry countryMadagascarDestination = FlightCountry.valueOf(countryMadagascar1.toUpperCase(Locale.ROOT));
+            String countryMadagascarDestination = countryMadagascar1.toUpperCase(Locale.ROOT);
+
+            System.out.println("Please enter your name:");
+            String name = details.nextLine();
+            //Passenger passenger1 = new Passenger(name);
+            System.out.println("Please enter your email:");
+            String email = details.nextLine();
+
+            System.out.println("Please enter your gender:");
+            String gender1 = details.nextLine();
+            Gender gender = Gender.valueOf(gender1);
+
+            System.out.println("Please enter your Passport Number:");
+            String passport = details.nextLine();
+
+            System.out.println("Please enter your Phone Number:");
+            Integer mobile = details.nextInt();
+
+            System.out.println("");
+            ID idGenerate = new ID();
+            StringBuilder id = idGenerate.generateId("ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890", 6);
+
+
+            //FlightBookingService service = new FlightBookingService();
+            //String country = service.getCountry();
+
+            //show all of the flight details and passenger details and ideally send this all in an email to user:
+            System.out.println("");
+            System.out.println("Here are your personal details:");
+            System.out.println("");
+            Passenger passenger = new Passenger(id, countryMadagascarDestination, name, email, gender, mobile, passport);
+            System.out.println(passenger.toString());
+            System.out.println("");
+            System.out.println("May The Odds Be Ever In Your Favour!");
+
+        } else {
+            System.out.println("GOT YA, ROBOT!");
+            System.out.println("");
+            flightBookingService.startBooking();
+
+        }
     }
+
+
+        public void enterPassengerDetailsSaudi() {
+
+            FlightBookingService flightBookingService = new FlightBookingService();
+
+            Scanner details = new Scanner(System.in);
+            System.out.println("Prove you're not a robot; enter your destination");
+            String countrySaudi1 = details.nextLine();
+
+            if (countrySaudi1.toUpperCase(Locale.ROOT).equals("PERU")) {
+                //FlightCountry countryPeruDestination = FlightCountry.valueOf(countryPeru1.toUpperCase(Locale.ROOT));
+                String countrySaudiDestination = countrySaudi1.toUpperCase(Locale.ROOT);
+
+                System.out.println("Please enter your name:");
+                String name = details.nextLine();
+                //Passenger passenger1 = new Passenger(name);
+                System.out.println("Please enter your email:");
+                String email = details.nextLine();
+
+                System.out.println("Please enter your gender:");
+                String gender1 = details.nextLine();
+                Gender gender = Gender.valueOf(gender1);
+
+                System.out.println("Please enter your Passport Number:");
+                String passport = details.nextLine();
+
+                System.out.println("Please enter your Phone Number:");
+                Integer mobile = details.nextInt();
+
+                System.out.println("");
+                ID idGenerate = new ID();
+                StringBuilder id = idGenerate.generateId("ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890", 6);
+
+
+//                FlightBookingService service = new FlightBookingService();
+//                String country = service.getCountry();
+
+                //show all of the flight details and passenger details and ideally send this all in an email to user:
+                System.out.println("");
+                System.out.println("Here are your personal details:");
+                System.out.println("");
+                Passenger passenger = new Passenger(id, countrySaudiDestination, name, email, gender, mobile, passport);
+                System.out.println(passenger.toString());
+                System.out.println("");
+                System.out.println("May The Odds Be Ever In Your Favour!");
+
+            } else {
+                System.out.println("GOT YA, ROBOT!");
+                System.out.println("");
+                flightBookingService.startBooking();
+
+            }
+        }
+
+
+    public void enterPassengerDetailsAustralia(){
+        FlightBookingService flightBookingService = new FlightBookingService();
+
+                Scanner details = new Scanner(System.in);
+                System.out.println("Prove you're not a robot; enter your destination");
+                String countryAustralia1 = details.nextLine();
+
+                if (countryAustralia1.toUpperCase(Locale.ROOT).equals("AUSTRALIA")) {
+                    //FlightCountry countryPeruDestination = FlightCountry.valueOf(countryPeru1.toUpperCase(Locale.ROOT));
+                    String countryAustraliaDestination = countryAustralia1.toUpperCase(Locale.ROOT);
+
+                    System.out.println("Please enter your name:");
+                    String name = details.nextLine();
+                    //Passenger passenger1 = new Passenger(name);
+                    System.out.println("Please enter your email:");
+                    String email = details.nextLine();
+
+                    System.out.println("Please enter your gender:");
+                    String gender1 = details.nextLine();
+                    Gender gender = Gender.valueOf(gender1);
+
+                    System.out.println("Please enter your Passport Number:");
+                    String passport = details.nextLine();
+
+                    System.out.println("Please enter your Phone Number:");
+                    Integer mobile = details.nextInt();
+
+                    System.out.println("");
+                    ID idGenerate = new ID();
+                    StringBuilder id = idGenerate.generateId("ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890", 6);
+
+
+                  // FlightBookingService service = new FlightBookingService();
+//                    String country = service.getCountry();
+
+                    //show all of the flight details and passenger details and ideally send this all in an email to user:
+                    System.out.println("");
+                    System.out.println("Here are your personal details:");
+                    System.out.println("");
+                    Passenger passenger = new Passenger(id, countryAustraliaDestination, name, email, gender, mobile, passport);
+                    System.out.println(passenger.toString());
+                    System.out.println("");
+                    System.out.println("May The Odds Be Ever In Your Favour!");
+
+
+                } else {
+                    System.out.println("GOT YA, ROBOT!");
+                    System.out.println("");
+                    flightBookingService.startBooking();
+
+                }}
+
+//    public String getCountry() {
+//        return country;
+//    }
+//
+//    public void setName(String country) {
+//        this.country = country;
+//    }
 }
 
 
--- Flight/src/main/java/com/psychedelicairlines/Passenger/PassengerService.java
+++ Flight/src/main/java/com/psychedelicairlines/Passenger/PassengerService.java
@@ -2,49 +2,70 @@ package com.psychedelicairlines.Passenger;
 
 import com.psychedelicairlines.Flights.FlightBookingService;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class PassengerService {
    // Passenger passenger1 = new Passenger({this.name});
+//
+//    public void enterPassengerDetails(){
+//
+//        Scanner details = new Scanner(System.in);
+//        System.out.println("Please enter your name:");
+//        String name = details.nextLine();
+//        //Passenger passenger1 = new Passenger(name);
+//        System.out.println("Please enter your email:");
+//        String email = details.nextLine();
+//
+//        System.out.println("Please enter your gender:");
+//        String gender1 = details.nextLine();
+//        Gender gender = Gender.valueOf(gender1);
+//
+//        System.out.println("Please enter your Passport Number:");
+//        String passport = details.nextLine();
+//
+//        System.out.println("Please enter your Phone Number:");
+//        Integer mobile = details.nextInt();
+//
+//        System.out.println("");
+//        ID idGenerate = new ID();
+//        StringBuilder id = idGenerate.generateId("ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890", 6);
+//
+//
+//        FlightBookingService service = new FlightBookingService();
+//        String country = service.getCountry();
+//
+//        //show all of the flight details and passenger details and ideally send this all in an email to user:
+//        System.out.println("");
+//        System.out.println("Here are your personal details:");
+//        System.out.println("");
+//        Passenger passenger = new Passenger(id, country, name, email, gender, mobile, passport);
+//        System.out.println(passenger.toString());
+//
+//
+//
+//
+//
+//
+//    }
 
-    public void enterPassengerDetails(){
-
-        Scanner details = new Scanner(System.in);
-        System.out.println("Please enter your name:");
-        String name = details.nextLine();
-        //Passenger passenger1 = new Passenger(name);
-        System.out.println("Please enter your email:");
-        String email = details.nextLine();
-
-        System.out.println("Please enter your gender:");
-        String gender1 = details.nextLine();
-        Gender gender = Gender.valueOf(gender1);
-
-        System.out.println("Please enter your Passport Number:");
-        String passport = details.nextLine();
-
-        System.out.println("Please enter your Phone Number:");
-        Integer mobile = details.nextInt();
-
-        System.out.println("");
-        ID idGenerate = new ID();
-        StringBuilder id = idGenerate.generateId("ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890", 6);
-
-
-        FlightBookingService service = new FlightBookingService();
-        String country = service.getCountry();
-
-        //show all of the flight details and passenger details and ideally send this all in an email to user:
-        System.out.println("");
-        System.out.println("Here are your personal details:");
-        System.out.println("");
-        Passenger passenger = new Passenger(id, country, name, email, gender, mobile, passport);
-        System.out.println(passenger.toString());
-
-
-
-
-
-    }
-
+//       if (countryPeru1.toUpperCase(Locale.ROOT).equals("PERU")) {
+////            //FlightCountry countryPeruDestination = FlightCountry.valueOf(countryPeru1.toUpperCase(Locale.ROOT));
+////            String countryPeruDestination = countryPeru1.toUpperCase(Locale.ROOT);
+////
+////        }else {
+////            System.out.println("Destinations doesn't match");
+////            displayFlights();
+////        }
+//
+//        switch(countryPeru1.toUpperCase(Locale.ROOT)){
+//            case "PERU":
+//                String countryPeruDestination = countryPeru1.toUpperCase(Locale.ROOT);
+//                break;
+//            default:
+//                System.out.println("Destinations doesn't match");
+//                displayFlights();
+//                break;
+//        }
+//
 }
